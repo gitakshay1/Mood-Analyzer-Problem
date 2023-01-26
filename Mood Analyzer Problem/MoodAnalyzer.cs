37bb091b4333c0fbe4d9d673f362025c68efb155
gitakshay1 <akshaypatil309p8@gmail.com>
2023-01-26 20:01:39 +0530

    [Akshay] added program for analyze the mood

---
+++ Mood Analyzer Problem/MoodAnalyzer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mood_Analyzer_Problem
+{
+    public class MoodAnalyzer
+    {
+        public string Analyzer(string Mood)
+        {
+            if(Mood.ToLower().Contains("happy"))
+            {
+                Console.WriteLine("Happy Mood");
+                return Mood.ToLower();
+            }
+            else
+            {
+                Console.WriteLine("Sad Mood");
+                return Mood.ToLower();
+            }
+        }
+    }
+}
---
+++ Mood Analyzer Problem/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Mood_Analyzer_Problem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            MoodAnalyzer moodAnalyzer= new MoodAnalyzer();
+            moodAnalyzer.Analyzer("I am sad");
+        }
+    }
+}
